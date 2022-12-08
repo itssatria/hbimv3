671d01fd513d2a8028d42afa12ef7edc0cf0ddba
itssatria <putrasatrianegara@gmail.com>
2022-12-08 12:48:00 +0700

    Babel Fixed

---
+++ build/precache-manifest.1a8f66c19d1ce9e1509bb7e740fad166.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ab29a6805e196a8209827a78c2a1a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "631cf4cb3ba140035bb2",
+    "url": "/static/css/2.d2aed415.chunk.css"
+  },
+  {
+    "revision": "c9fc24d0cd40094ab0fc",
+    "url": "/static/css/main.4b350f01.chunk.css"
+  },
+  {
+    "revision": "631cf4cb3ba140035bb2",
+    "url": "/static/js/2.2e63a14d.chunk.js"
+  },
+  {
+    "revision": "b1f13ebe33e8a710e4b4400a9fb7a5c3",
+    "url": "/static/js/2.2e63a14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fc24d0cd40094ab0fc",
+    "url": "/static/js/main.d2a1f135.chunk.js"
+  },
+  {
+    "revision": "e6aa33f31165fc90b992",
+    "url": "/static/js/runtime-main.0543246b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
